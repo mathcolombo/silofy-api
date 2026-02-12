c1a6e6de8622f526c99323e68f70f821aedce88f
Matheus Colombo <111023158+mathcolombo@users.noreply.github.com>
2026-02-11 23:23:11 -0300

    SLF-01 - Adiciona swagger no projeto de API

---
+++ src/Silofy.Api/Configs/SwaggerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.OpenApi.Models;
+
+namespace Silofy.Api.Configs;
+
+public static class SwaggerConfiguration
+{
+    public static void AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Silofy-api", Version = "v1" });
+    
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
+    }
+}
---
+++ src/Silofy.IoC/Configs/HttpConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Silofy.IoC.Configs;
+
+public static class HttpConfiguration
+{
+    public static IServiceCollection AddHttpConfiguration(this IServiceCollection services)
+    {
+        return services;
+    }
+}
